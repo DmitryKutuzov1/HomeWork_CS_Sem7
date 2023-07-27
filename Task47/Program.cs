29de71fa1ab92da115f5132713ff64376b0250fd
Dmitry <dimakutuzov1234@gmail.com>
2023-07-27 21:35:36 +0400

    Task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void Print2DArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] Fill2DArray(int rows, int columns, int startValue, int finishValue)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().Next(startValue, finishValue) + new Random().NextDouble(), 2);
+        }
+    }
+    return matrix;
+}
+
+int GetImput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int m = GetImput("Введите колличество строк: ");
+int n = GetImput("Введите колличество столбцов: ");
+double[,] array = Fill2DArray(m, n, -10, 10);
+Print2DArray(array);
